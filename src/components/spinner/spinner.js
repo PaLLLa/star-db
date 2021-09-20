9f36cb488c6b0f4a9689897d190b67fffbdfdafb
PaLLLa <p.debelko@gmail.com>
2021-09-20 20:27:36 +0300

    Made a structure of project

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './spinner.css';
+
+export default class Spinner extends Component {
+
+    render(){
+        return (
+            <div className="loadingio-spinner-double-ring-gqlzux6ut45">
+                <div className="ldio-j538lc2bdy">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+        
+    }
+
+}
